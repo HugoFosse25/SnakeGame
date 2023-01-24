c5b5e6c770cd6174182a39e4219c5bee3f1a1a44
Hugo Fossé <fosse.hugo25@gmail.com>
2023-01-24 10:54:23 +0100

    Update Snake.cs
    Modification des contrôles du Serpent (Quand il vas à gauche il ne peut pas aller à droite et inversement)

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -12,19 +12,19 @@ public class Snake : MonoBehaviour {
 
     void Update() {
     
-        if(Input.GetKeyDown(KeyCode.UpArrow))   //Pour gérer les mouvements du serpent
+        if(Input.GetKeyDown(KeyCode.UpArrow) && dir.x != 0)   //Pour gérer les mouvements du serpent
         {
             dir = Vector2.up;
         }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        else if(Input.GetKeyDown(KeyCode.DownArrow) && dir.x != 0)
         {
             dir = Vector2.down;
         }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && dir.y != 0)
         {
             dir = Vector2.left;
         }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && dir.y != 0)
         {
             dir = Vector2.right;
         }
